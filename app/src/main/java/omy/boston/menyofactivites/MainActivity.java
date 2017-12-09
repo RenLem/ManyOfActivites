dc64a701de891595f00299eef694fb293b47e56b
Luij Del Mar - Renato Puškarić <codetodesign.cc@gmail.com>
2017-12-09 23:12:46 +0100

    Initial commit

---
+++ app/src/main/java/omy/boston/menyofactivites/MainActivity.java
@@ -0,0 +1,26 @@
+package omy.boston.menyofactivites;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnOneActivity = (Button) findViewById(R.id.btnActivity);
+
+        btnOneActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
